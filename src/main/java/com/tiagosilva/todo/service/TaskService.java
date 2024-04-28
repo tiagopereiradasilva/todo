a0bdac160d362a69b2b8f1fdf59d3f3fa71f6da8
Tiago Silva <tiagopereiracx@gmail.com>
2024-04-27 22:35:26 -0300

    feat: inclusão de logs

--- src/main/java/com/tiagosilva/todo/service/TaskService.java
+++ src/main/java/com/tiagosilva/todo/service/TaskService.java
@@ -6,13 +6,16 @@ import com.tiagosilva.todo.enums.EnumTaskStatus;
 import com.tiagosilva.todo.exceptions.TaskNotFoundException;
 import com.tiagosilva.todo.exceptions.UpdateStatusException;
 import com.tiagosilva.todo.repository.TaskRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@Slf4j
 public class TaskService {
+
     private final TaskRepository taskRepository;
 
     public TaskService(TaskRepository taskRepository) {
@@ -20,15 +23,18 @@ public class TaskService {
     }
 
     public List<Task> findAll() {
+        log.info("Buscando todos as tarefas cadastradas.");
         return  taskRepository.findAll();
     }
 
 
     public Task save(TaskDto dto) {
+        log.info("Salvando Task no banco.");
         return taskRepository.save(toTask(dto));
     }
 
     private Task toTask(TaskDto dto) {
+        log.info("Convertendo payload em nova Task.");
         return Task.builder()
                 .title(dto.title())
                 .description(dto.description())
@@ -39,6 +45,7 @@ public class TaskService {
     }
 
     public void update(String id, TaskDto dto) {
+        log.info("Atualizando Task - id {}", id);
         Task task = taskRepository.findById(id).orElseThrow(() -> new TaskNotFoundException(String.format("Task %s não encontrada", id)));
         task.setTitle(dto.title());
         task.setDescription(dto.description());
@@ -47,11 +54,13 @@ public class TaskService {
     }
 
     public void delete(String id) {
+        log.info("Deletando Task - id {}", id);
         Task task = taskRepository.findById(id).orElseThrow(() -> new TaskNotFoundException(String.format("Task %s não encontrada", id)));
         taskRepository.delete(task);
     }
 
     public void updateStatus(String id, EnumTaskStatus status) {
+        log.info("Inicio atualização de status da Task {} para {}", id, status);
         taskRepository.findById(id)
                 .map(
                         t -> {
@@ -61,6 +70,7 @@ public class TaskService {
                                 taskRepository.save(t);
                                 return t;
                             }
+                            log.error("Não foi possível atualizar Task {} para {}", id, status);
                             throw new UpdateStatusException(String.format("Erro ao alterar status %s para %s da Task %s", t.getStatus(), status, id));
                         }
                 ).orElseThrow(() -> new TaskNotFoundException(String.format("Task %s não encontrada", id)));
@@ -68,6 +78,7 @@ public class TaskService {
     }
 
     private boolean validateUpdateStatus(EnumTaskStatus status, EnumTaskStatus statusPassed){
+        log.info("Validando atualização de status da Task de {} para {}", status, statusPassed);
         switch (status){
             case BACKLOG, PAUSED -> {
                 return statusPassed == EnumTaskStatus.DOING;
